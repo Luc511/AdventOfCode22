56166677940770ca0bfc2d1a142e0acb16c94208
Lucas Balon <lucasbalon@gmail.com>
2022-12-05 09:32:34 +0100

    Day 4 Part 2 attempt

--- day4/Part2.java
+++ day4/Part2.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class Part1 {
+public class Part2 {
 
     public static void main(String[] args) throws FileNotFoundException {
         FileReader fl = new FileReader("/Users/lucasbalon/Documents/VSCode/AdventOfCode22/day4/data.txt");
@@ -12,6 +12,8 @@ public class Part1 {
         int section12 = 0;
         int section21 = 0;
         int section22 = 0;
+        int[] interval1;
+        int[] interval2;
         int count = 0;
 
         while (scan.hasNextLine()) {
@@ -20,6 +22,18 @@ public class Part1 {
             section12 = Integer.parseInt(line.substring(line.indexOf('-')+1, line.indexOf(',')));
             section21 = Integer.parseInt(line.substring(line.indexOf(',')+1, line.indexOf('-', line.indexOf(','))));
             section22 = Integer.parseInt(line.substring(line.indexOf('-', line.indexOf(','))+1));
+            interval1 = createInterval(section11, section12);
+            interval2 = createInterval(section21, section22);
+
+            outer:
+            for (int i : interval1) {
+                for (int j : interval2) {
+                    if (i == j) {
+                        count++;
+                        break outer;
+                    }
+                }
+            }
 
             if (true) {
                 count++;
@@ -27,5 +41,15 @@ public class Part1 {
         }
         scan.close();
         System.out.println(count);
+    }
+
+    public static int[] createInterval(int section1, int section2) {
+        int[] interval = new int[section2 - section1];
+        int j = 0;
+        for (int i = section1; j < interval.length; i++) {
+            interval[j] = i;
+            j++;
+        }
+        return interval;
     }
 }
